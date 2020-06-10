2089090ee726117b0ea58d13438b8392c1175831
XCore64 <23143014+XCore64@users.noreply.github.com>
2020-06-10 17:28:06 +0000

    Add files via upload
    Debian Buster install

---
+++ ovpn_apt_repo_debianbuster.sh
@@ -0,0 +1,11 @@
+#Installs prerequisites
+sudo apt update && sudo apt -y install ca-certificates wget net-tools gnupg;
+
+#GPG Key
+wget -qO - https://as-repository.openvpn.net/as-repo-public.gpg | sudo apt-key add -;
+
+#Adds OpenVPN Repo to the sources
+echo "deb http://as-repository.openvpn.net/as/debian buster main" | sudo tee  /etc/apt/sources.list.d/openvpn-as-repo.list;
+
+#Updates repositories and installs OpenVPN Access Server through repo
+sudo apt update && sudo apt -y install openvpn-as;
